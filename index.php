732e39439ddca1d7fbdc19f4792ec457c9217f98
Shaun <shaun@thenetninja.co.uk>
2019-01-10 11:29:29 +0000

    lesson-10

--- index.php
+++ index.php
@@ -1,38 +1,34 @@
 <?php 
 
-	$ninjas = ['shaun', 'ryu', 'yoshi'];
+	// comparisons & booleans (true or false)
+	//echo true // echo's "1"
+	//echo false // echo's ""
 
-	// for($i = 0; $i < count($ninjas); $i++){
-	// 	echo $ninjas[$i] . '<br />';
-	// }
+	// numbers
+	//echo 5 < 10;
+	//echo 5 > 10;
+	//echo 5 == 10;
+	//echo 10 == 10;
+	//echo 5 != 10;
+	//echo 5 <= 5;
+	//echo 5 >= 5;
 
-	// foreach($ninjas as $ninja){
-	// 	echo $ninja . ' <br/>';
-	// }
+	// strings
+	//echo 'shaun' < 'yoshi';
+	//echo 'shaun' > 'yoshi';
+	//echo 'shaun' > 'Shaun';
+	//echo 'mario' == 'mario';
+	//echo 'mario' == 'Mario';
 
-	$products = [
-		['name' => 'shiny star', 'price' => 20],
-		['name' => 'green shell', 'price' => 10],
-		['name' => 'red shell', 'price' => 15],
-		['name' => 'gold coin', 'price' => 5],
-		['name' => 'lightning bolt', 'price' => 40],
-		['name' => 'banana skin', 'price' => 2]
-	];
+	// loose vs strict equal comparison
 
-	// foreach($products as $product){
-	// 	echo $product['name'] .' - '. $product['price'];
-	// 	echo '<br />';
-	// }
+	//echo 5 == '5';
+	//echo 5 === '5';
+	//echo 5 === 5;
 
-	$i = 0;
-
-	while($i < count($products)){
-		echo $products[$i]['name'];
-		echo '<br />';
-		$i++;
-	}
-
-
+	//echo true == 1;
+	echo true === 1;
+	
 ?>
 
 <!DOCTYPE html>
@@ -41,14 +37,6 @@
 	<title>PHP Tutorials</title>
 </head>
 <body>
-
-	<h1>Products</h1>
-	<ul>
-		<?php foreach($products as $product){ ?>
-			<h3><?php echo $product['name']; ?></h3>
-			<p>£<?php echo $product['price']; ?></p>
-		<?php } ?>
-	</ul>
 
 </body>
 </html>
