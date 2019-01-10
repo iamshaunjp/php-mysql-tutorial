8d768bd3ef08d415c82411fe0ad358b4a35f17b2
Shaun <shaun@thenetninja.co.uk>
2019-01-10 17:08:58 +0000

    lesson-20

--- add.php
+++ add.php
@@ -1,26 +1,37 @@
 <?php 
 
 	if(isset($_POST['submit'])){
+
+		$email = $title = $ingredients = '';
 		
 		// check email
 		if(empty($_POST['email'])){
 			echo 'An email is required <br />';
 		} else{
-			echo htmlspecialchars($_POST['email']) . '<br />';
+			$email = $_POST['email'];
+			if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+				echo 'Email must be a valid email address';
+			}
 		}
 
 		// check title
 		if(empty($_POST['title'])){
 			echo 'A title is required <br />';
 		} else{
-			echo htmlspecialchars($_POST['title']) . '<br />';
+			$title = $_POST['title'];
+			if(!preg_match('/^[a-zA-Z\s]+$/', $title)){
+				echo 'Title must be letters and spaces only';
+			}
 		}
 
 		// check ingredients
 		if(empty($_POST['ingredients'])){
 			echo 'At least one ingredient is required <br />';
 		} else{
-			echo htmlspecialchars($_POST['ingredients']) . '<br />';
+			$ingredients = $_POST['ingredients'];
+			if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)){
+				echo 'Ingredients must be a comma separated list';
+			}
 		}
 
 	} // end POST check
