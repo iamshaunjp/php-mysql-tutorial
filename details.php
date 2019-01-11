d9942afb96964aedc61f1ae9bdd5bfe66472f83f
Shaun <shaun@thenetninja.co.uk>
2019-01-11 13:16:46 +0000

    lesson-32

--- details.php
+++ details.php
@@ -2,6 +2,20 @@
 
 	include('config/db_connect.php');
 
+	if(isset($_POST['delete'])){
+
+		$id_to_delete = mysqli_real_escape_string($conn, $_POST['id_to_delete']);
+
+		$sql = "DELETE FROM pizzas WHERE id = $id_to_delete";
+
+		if(mysqli_query($conn, $sql)){
+			header('Location: index.php');
+		} else {
+			echo 'query error: '. mysqli_error($conn);
+		}
+
+	}
+
 	// check GET request id param
 	if(isset($_GET['id'])){
 		
@@ -36,6 +50,13 @@
 			<p><?php echo date($pizza['created_at']); ?></p>
 			<h5>Ingredients:</h5>
 			<p><?php echo $pizza['ingredients']; ?></p>
+
+			<!-- DELETE FORM -->
+			<form action="details.php" method="POST">
+				<input type="hidden" name="id_to_delete" value="<?php echo $pizza['id']; ?>">
+				<input type="submit" name="delete" value="Delete" class="btn brand z-depth-0">
+			</form>
+
 		<?php else: ?>
 			<h5>No such pizza exists.</h5>
 		<?php endif ?>
