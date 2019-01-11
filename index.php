d336b54351878d6283db6d93c94439eaeab3267f
Shaun <shaun@thenetninja.co.uk>
2019-01-11 11:16:00 +0000

    lesson-26

--- index.php
+++ index.php
@@ -1,5 +1,31 @@
 <?php 
 
+	// connect to the database
+	$conn = mysqli_connect('localhost', 'shaun', 'test1234', 'ninja_pizza');
+
+	// check connection
+	if(!$conn){
+		echo 'Connection error: '. mysqli_connect_error();
+	}
+
+	// write query for all pizzas
+	$sql = 'SELECT title, ingredients, id FROM pizzas ORDER BY created_at';
+
+	// get the result set (set of rows)
+	$result = mysqli_query($conn, $sql);
+
+	// fetch the resulting rows as an array
+	$pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+	// free the $result from memory (good practise)
+	mysqli_free_result($result);
+
+	// close connection
+	mysqli_close($conn);
+
+	print_r($pizzas);
+
+
 ?>
 
 <!DOCTYPE html>
