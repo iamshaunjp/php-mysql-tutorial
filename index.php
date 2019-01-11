382f8b29dbd981208682a115ea9e933dc1d272ce
Shaun <shaun@thenetninja.co.uk>
2019-01-11 12:06:31 +0000

    lesson-27

--- index.php
+++ index.php
@@ -23,8 +23,6 @@
 	// close connection
 	mysqli_close($conn);
 
-	print_r($pizzas);
-
 
 ?>
 
@@ -32,6 +30,30 @@
 <html>
 	
 	<?php include('templates/header.php'); ?>
+
+	<h4 class="center grey-text">Pizzas!</h4>
+
+	<div class="container">
+		<div class="row">
+
+			<?php foreach($pizzas as $pizza){ ?>
+
+				<div class="col s6 md3">
+					<div class="card z-depth-0">
+						<div class="card-content center">
+							<h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
+							<div><?php echo htmlspecialchars($pizza['ingredients']); ?></div>
+						</div>
+						<div class="card-action right-align">
+							<a class="brand-text" href="#">more info</a>
+						</div>
+					</div>
+				</div>
+
+			<?php } ?>
+
+		</div>
+	</div>
 
 	<?php include('templates/footer.php'); ?>
 
