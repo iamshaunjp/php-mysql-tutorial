1726838d22ca4509e758d6c4cdbef7eaa044ce84
Shaun <shaun@thenetninja.co.uk>
2019-01-14 17:23:41 +0000

    lesson-42

--- sandbox.php
+++ sandbox.php
@@ -2,8 +2,8 @@
 
 	class User {
 
-		public $email;
-		public $name;
+		private $email;
+		private $name;
 
 		public function __construct($name, $email){
 			// $this->name = 'mario';
@@ -17,18 +17,28 @@
 			echo $this->name . ' logged in';
 		}
 
-	}
+		public function getName(){
+			return $this->name;
+		}
 
-	// $userOne = new User();
-	
-	// $userOne->login();
-	// echo $userOne->name;
+		public function setName($name){
+			if(is_string($name) && strlen($name) > 1){
+				$this->name = $name;
+				return "name updated to $name";
+			} else {
+				return 'not a valid name';
+			}
+		}
+
+	}
 
 	$userTwo = new User('yoshi', 'yoshi@thenetninja.co.uk');
 
-	// echo $userTwo->email;
-	$userTwo->login();
+	// $userTwo->name = 'mario';
+	// echo $userTwo->name;
 
-
+	// echo $userTwo->getName();
+	// echo $userTwo->setName('shaun');
+	// echo $userTwo->getName();
 
 ?>
